f6a3c9f3cb526eb1e938f9c67bcac936097c2f51
Dima-Yu <mindwork.one@gmail.com>
2021-06-08 11:31:27 +0300

    add friendship table and friends search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: %i[create destroy]
+  resources :user_friends, only: %i[create destroy]
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
+  get 'search_friend', to: 'users#search'
 end
